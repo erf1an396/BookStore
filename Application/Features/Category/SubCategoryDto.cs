990ecbd5f66510c9e6405366adbbbd7facd58846
erf1an396 <em.karfarma@gmail.com>
2025-05-12 20:18:08 +0330

    add login in home on user icon , add subCategoty in home

---
+++ Application/Features/Category/SubCategoryDto.cs
@@ -0,0 +1,23 @@
+﻿using Application.Mappings;
+using Application.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Category
+{
+    public class SubCategoryDto : BaseDto , IMapFrom<Domain.Entities.Category>
+    {
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Domain.Entities.Category, CategoryDto>()
+                .ReverseMap();
+        }
+
+        public string Title { get; set; }
+
+    }
+}
